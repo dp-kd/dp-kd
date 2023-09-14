2bc9de7ae93696db11d18b12351c971b86754581
dp-kd <139973649+dp-kd@users.noreply.github.com>
2023-09-14 19:06:13 +0530

    Create_form_select.php

---
+++ form_select.php
@@ -0,0 +1,9 @@
+<?php
+$un=$_POST['username'];
+$pwd=$_POST['password'];
+$sql="select * from register where username='$un' and password='$pwd';
+$result=mysqli_query($con,$sql);
+$count=mysqli_num_rows($result);
+if($count>0){
+
+?>
